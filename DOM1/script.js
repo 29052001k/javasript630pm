d16b3868e43872c6c81f9ff3af78cbb8491d7bd5
29052001k <kalyanihadole2001@gmail.com>
2023-03-29 11:32:40 +0530

    DOM

---
+++ DOM1/script.js
@@ -0,0 +1,18 @@
+document.querySelector('h1')
+.addEventListener('click',function(){
+    document.querySelector('h1').style.color="green"
+})
+
+//tag name
+let tagN=document.querySelector('p')
+console.log(tagN)
+//id
+let idN=document.querySelector('#four')
+console.log(idN)
+//class
+let classN=document.querySelector('.five')
+console.log(classN)
+//common formula
+//[attribute:"value"]
+//let attN=document.querySelector('p[name="six"]')
+
