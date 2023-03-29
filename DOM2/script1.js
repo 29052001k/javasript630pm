a28ccb9de5a5333ad4f4a03b58185aa19429a5b4
29052001k <kalyanihadole2001@gmail.com>
2023-03-29 11:33:15 +0530

    DOM

---
+++ DOM2/script1.js
@@ -0,0 +1,61 @@
+//<h1 class="one" id="two" name="three">Heading</h1>
+//tag name
+let bytag = document.querySelector('h1')
+console.log(bytag)
+//class
+let byclass = document.querySelector('.one')
+console.log(byclass)
+//id
+let byid = document.querySelector('#two')
+console.log(byid)
+//common formula
+let tagN = document.querySelector('h1[name="three"]')
+console.log(tagN)
+
+//program1(index1.html)
+
+//   tagN.addEventListener('click',function(){
+//     tagN.style.color='green'
+//     tagN.textContent="Kalyani"
+//   })
+
+// program2 (index2.html)
+
+// let Heading1=document.querySelector('h1')
+// let button1=document.querySelector('button')
+// button1.addEventListener('click',function(){
+//     Heading1.style.color='green'
+//     Heading1.textContent='Amruta'
+// })
+
+//program 3(index3.html)
+
+// let Heading1=document.querySelector('h1')
+// let button1=document.querySelector('button')
+// let inputtext=document.querySelector('input')
+// button1.addEventListener('click',function(){
+// let colortext=inputtext.value
+//  Heading1.style.color=colortext
+//  inputtext.value=" "
+// })
+
+
+// let Heading1 = document.querySelector('h1')
+// let button1 = document.querySelector('button')
+// let inputtext = document.querySelector('input')
+// button1.addEventListener('click', function () {
+//     let colortext = inputtext.value
+//     Heading1.style.color = colortext
+//     inputtext.value = " "
+//     Heading1.textContent = "Aryan"
+// })
+
+
+
+
+
+
+
+
+
+
