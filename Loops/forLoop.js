6742626971e51593bdf2687a02ab560804cf3490
29052001k <kalyanihadole2001@gmail.com>
2023-03-29 12:20:14 +0530

    loops

---
+++ Loops/forLoop.js
@@ -0,0 +1,107 @@
+//Guess Number Infinite Time Until User Guess Correct Number
+// let randomNumber = Math.floor(Math.random()*6) + 1
+// //console.log(randomNumber) 
+// let userInput;
+// while(randomNumber!=userInput){
+//     randomNumber = Math.floor(Math.random()*6) + 1
+//     console.log(randomNumber) 
+//         userInput = prompt("Enter any number between 1 to 6")
+//     if(randomNumber == userInput){
+//         console.log('Guess is correct')
+//         break
+//     }
+//     else {
+//         console.log('Guess is incorrect , please try again !')
+//     }
+// }
+
+
+// let randomN = Math.floor(Math.random() * 6 + 1)
+// console.log(randomN)
+// let UserInput;
+
+// for (let i = 0; i < 5; i++) {
+//     UserInput = prompt('enter the number between 1 to 6')
+//  } if (randomN == UserInput) {
+//         console.log("guess is correct")
+//     } else {
+//         console.log("guess is incorrect")
+//     }
+
+
+// let randomN=Math.floor(Math.random()*6+1)
+// console.log(randomN)
+// let UserInput;
+// console.log(UserInput)
+
+// while(randomN!=UserInput){
+//     UserInput=prompt('enter the number between 1 to 6')
+// } if (randomN==UserInput){
+//     console.log("guess is correct")
+// }else{
+//     console.log("guess is incorrect")
+// }
+
+
+
+// let randomN=Math.floor(Math.random()*6+1)
+// console.log(randomN)
+// let UserInput;
+
+// for(let i=0;i<5;i++){
+//     UserInput=prompt("enter the number between 1 to 6")  
+// }if (UserInput==randomN){
+//     console.log("guess is correct")
+//break;
+// }else{
+//     console.log("guess is correct")
+// }
+
+// let randomN=Math.floor(Math.random()*6+1)
+// console.log(randomN)
+//  let UserInput;
+// console.log(UserInput)
+
+// while(UserInput!=randomN){
+//     UserInput=prompt("enter the number between 1 to 6")
+// }if (UserInput==randomN){
+//     console.log("guess is correct")
+// }else{
+//     console.log("guess is incorrect")
+// }
+
+
+// let randomN =Math.floor(Math.random()*6+1)
+// console.log(randomN)
+// let UserInput;
+
+// while(UserInput!=randomN){
+//     UserInput=prompt("enter the number between 1 to 6")
+// }if (UserInput==randomN){
+//     console.log("your guess is correct")
+// }else{
+//     console.log("your guess is incorrect")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
