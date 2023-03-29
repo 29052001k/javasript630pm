53b278e858350d03aaabce63404e8118d5a12fb8
29052001k <kalyanihadole2001@gmail.com>
2023-03-29 11:10:12 +0530

    DOM

---
+++ DOM/script.js
@@ -0,0 +1,17 @@
+document.querySelector('h1')
+.addEventListener('click', function(){
+    document.querySelector('h1').textContent=document.querySelector('h1').textContent.toUpperCase()
+})
+// document.querySelector('h1')
+// .addEventListener('click',function(){
+// document.querySelector('h1').textContent=document.querySelector('h1').textContent.toUpperCase()
+// })
+// Document.querySelector('h1')
+// .addEventListener('click',function(){
+//     Document.querySelector('h1').textContent=Document.querySelector('h1').textContent.toUpperCase()  
+// })
+
+// document.querySelector('h1')
+// .addEventListener('click',function(){
+//     document.querySelector('h1').textContent=document.querySelector('h1').textContent.toUpperCase()
+// })
